1d09563a93e6c5508c16c23592e5b33ac2a092b3
hollyxj <hmj19a@fsu.edu>
2023-02-07 19:22:18 -0500

    Rating component implemented

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return <footer>
+    <Container>
+      <Row>
+        <Col className='text-center py-3'>
+          Copyright &copy; HollyShop 2023
+        </Col>
+      </Row>
+    </Container>
+  </footer>
+}
+
+export default Footer
