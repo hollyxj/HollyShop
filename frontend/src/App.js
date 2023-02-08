cc16b3f3ca74350e678303f87f1036155123877b
hollyxj <hmj19a@fsu.edu>
2023-02-07 19:49:15 -0500

    Implemented React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
-    <Header/>
-    
-    <main className='py-3'>
-      <Container>
-        <h1>Welcome to ShopHolly.com!</h1>
-        <HomeScreen/>
-      </Container>
-    
-    </main>
-    <Footer/>
-    </>
+    <Router>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ShopHolly.com!</h1>
+          <Routes>         
+            <Route path='/' element={<HomeScreen/>} exact/>
+          </Routes>
+          <Routes>         
+            <Route path='/product/:id' element={<ProductScreen/>}/>
+          </Routes>
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
 import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
     <Card className='my=3 p-3 rounded'>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
             <Card.Img src={product.image} variant='top'/>
-        </a>
+        </Link>
         <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
             <Card.Title as='div'>
                 <strong>{product.name}</strong>
             </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as='div'>
             <div className='my-3'>
@@ -25,10 +26,7 @@ const Product = ({ product }) => {
         </Card.Text>
 
         <Card.Text as='h3'>${product.price}
-
-
         </Card.Text>
-
       </Card.Body>
     </Card>
   )
