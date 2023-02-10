4e81bd8270755e8c546f479a785c1b69af6ee9b8
hollyxj <hmj19a@fsu.edu>
2023-02-10 11:47:58 -0500

    Product List Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk' 
+import { productListReducer } from './reducers/productReducers'
  
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        productList: productListReducer
+    },
     preloadedState: {},
     middleware: [thunk],
 })
