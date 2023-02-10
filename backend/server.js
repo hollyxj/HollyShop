5031047f06411e19017c7af4d6942708d96b1ed7
hollyxj <hmj19a@fsu.edu>
2023-02-10 11:08:26 -0500

    Custom Error Handling implemented

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -16,7 +17,11 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
 const PORT = process.env.PORT || 4000
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT), console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.magenta.bold)
 
