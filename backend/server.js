b62bfe018514a6858ca131d603474276419b08f1
hollyxj <hmj19a@fsu.edu>
2023-02-09 20:50:02 -0500

    Set up environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
+
+dotenv.config()
 
 const app = express()
 
@@ -16,5 +19,7 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 })
 
-app.listen(4000), console.log('Server running on port 4000')
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT), console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 
