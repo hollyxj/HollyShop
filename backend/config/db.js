6a7aa9af84f411a09c3052f734d5e05528a93a2a
hollyxj <hmj19a@fsu.edu>
2023-02-09 21:18:45 -0500

    Connecting to the MongoDB Database

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+// Silence warning
+mongoose.set('strictQuery', true)
+
+const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+    }
+
+}
+
+export default connectDB
