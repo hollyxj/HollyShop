3b7fb07a4196db80902639860c4e55cf1134f008
hollyxj <hmj19a@fsu.edu>
2023-02-10 12:08:51 -0500

    Product Details Reducer & Action

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product.js'
+import Message from '../components/Message.js'
+import Loader from '../components/Loader.js'
 import { listProducts } from '../actions/productActions.js'
 
 const HomeScreen = () => {
@@ -18,11 +20,10 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       { loading ? ( 
-         <h2>Loading...</h2> 
+         <Loader />
       ) : error  ? ( 
-        <h3>{error}</h3> 
-      )
-      : ( 
+        <Message variant='danger'>{error}</Message> 
+      ) : ( 
         <Row>
           {products.map(product => 
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
