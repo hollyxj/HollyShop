f55663deafd617eadfdb0c2e905a56cd5734f76a
hollyxj <hmj19a@fsu.edu>
2023-02-10 15:40:12 -0500

    Warning: Each child in a list should have a unique key prop. Subtotal not working

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,21 +1,118 @@
-import React, { useState, useEffect } from 'react'
-import { Link, useNavigate, useParams, useLocation } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useParams, useNavigate, useLocation, Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap' 
+import { Row, Col, Image, ListGroup, Card, Button, Form, Nav } from 'react-bootstrap' 
 import Message from '../components/Message'
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
+
 
 const CartScreen = () => {
-  const { productId } = useParams()
+  const { id } = useParams()
+  //const[searchParams] = useSearchParams()
   const location = useLocation()
   const qty = new URLSearchParams(location.search).get('qty')
 
-  console.log(qty)
+  console.log(`id: ${id}`)
+  console.log(`qty: ${qty}`)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(addToCart(id, qty))
+  },[dispatch, id, qty])
+
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
+
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id))
+  }
+
+  const navigate = useNavigate()
+
+  const checkoutHandler = () => {
+    navigate('/login?redirect=shipping')
+  }
 
   return (
-    <div>
-        Cart Screen
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as='select'
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Proceed To Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
   )
 }
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,8 +3,7 @@ import thunk from 'redux-thunk'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
  
-const cartItemsFromStorage = 
-    localStorage.getItem('cartItems') 
+const cartItemsFromStorage = localStorage.getItem('cartItems') 
     ? JSON.parse(localStorage.getItem('cartItems'))
     : []
 
