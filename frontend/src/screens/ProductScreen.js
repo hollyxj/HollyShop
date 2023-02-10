e855f54d130002f5f80fb46b02d30cdb8647619c
hollyxj <hmj19a@fsu.edu>
2023-02-10 12:22:41 -0500

    Product Details Reducer & Action

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,25 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap' 
 import Rating from '../components/Rating'
-import axios from 'axios'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProductDetails } from '../actions/productActions'
+
 
 const ProductScreen = ({ match }) => {
     const { id } = useParams()
-    const [product, setProduct] = useState({})
+
+    const dispatch = useDispatch()
+
+    const productDetails = useSelector(state => state.productDetails)
+    const { loading, error, product } = productDetails
 
     useEffect(() =>  {
-        const fetchProduct = async() => {
-          const { data } = await axios.get(`/api/products/${id}`)
-          
-          setProduct(data)
-        }
-        fetchProduct()
-      }, [ id ])
+        dispatch(listProductDetails(id))
+    }, [ dispatch, id ])
+
 
     return<>
-        <Link className='btn btn-secondary my-3' to='/'>Go Back</Link>
-        <Row>
+        <Link className='btn btn-secondary my-3' to='/'>
+            Go Back
+        </Link>
+        {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
+            <Row>
             <Col md={6}>
                 <Image src={product.image} alt={product.name} fluid/>
             </Col>
@@ -74,6 +81,9 @@ const ProductScreen = ({ match }) => {
                 </Card>
             </Col>
         </Row>
+
+        )}
+        
     </>
 }
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk' 
-import { productListReducer } from './reducers/productReducers'
+import { productListReducer, productDetailsReducer } from './reducers/productReducers'
  
 const store = configureStore({
     reducer: {
-        productList: productListReducer
+        productList: productListReducer,
+        productDetails: productDetailsReducer
     },
     preloadedState: {},
     middleware: [thunk],
 })
- 
+
 export default store
