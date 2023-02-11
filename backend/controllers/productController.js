a45e653bac3124d94bbefa58721b152cb41e6267
hollyxj <hmj19a@fsu.edu>
2023-02-10 21:55:01 -0500

    User Authentication Endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import Product from  '../models/productModel.js'
 
-
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
