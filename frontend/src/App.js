6fb25555648e24d207c2d4bd493a4b4c3f86b9d9
hollyxj <hmj19a@fsu.edu>
2023-02-13 21:32:48 -0500

    User Register Reducer, Action & Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <h1>Welcome to Holly Shop!</h1>
           <Routes>         
             <Route path='/login' element={<LoginScreen />}/>
+            <Route path='/register' element={<RegisterScreen />}/>
             <Route path='/product/:id' element={<ProductScreen />}/>
             <Route path='/cart'>
               <Route path=':id' element={<CartScreen />} />
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk' 
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') 
     ? JSON.parse(localStorage.getItem('cartItems'))
@@ -18,6 +18,7 @@ const store = configureStore({
         productDetails: productDetailsReducer,
         cart: cartReducer,
         userLogin: userLoginReducer,
+        userRegister: userRegisterReducer,
     },
     preloadedState: {
         cart: { cartItems: cartItemsFromStorage },
