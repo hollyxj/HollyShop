d3f87900ad324c60ddd841ef2933989154da8bb6
hollyxj <hmj19a@fsu.edu>
2023-02-13 21:44:34 -0500

    Update Profile Endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,13 @@ import express from 'express'
 const router = express.Router()
 import { authUser, registerUser, getUserProfile } from '../controllers/userController.js' 
 import { protect } from '../middleware/authMiddleware.js'
+import { updateUserProfile } from '../controllers/userController.js'
 
 router.route('/').post(registerUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router
+    .route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile)
 
 export default router
