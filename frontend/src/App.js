4d5eccccc4e83b3b9ab6099d010f9000881ce803
hollyxj <hmj19a@fsu.edu>
2023-02-16 12:29:09 -0500

    Admin Product List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 const App = () => {
   return (
@@ -32,6 +33,7 @@ const App = () => {
             <Route path='/profile' element={<ProfileScreen />}/>
             <Route path='/admin/userlist' element={<UserListScreen />}/>
             <Route path='/admin/user/:id/edit' element={<UserEditScreen />}/>
+            <Route path='/admin/productlist' element={<ProductListScreen />}/>
             <Route path='/product/:id' element={<ProductScreen />}/>
             <Route path='/cart'>
               <Route path=':id' element={<CartScreen />} />
