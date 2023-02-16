3ff1e51f630e54e84ddda3c47e25d271d41a2c50
hollyxj <hmj19a@fsu.edu>
2023-02-16 15:24:08 -0500

    Admin order list: Showing empty table unpopulated with data yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
 import ProductEditScreen from './screens/ProductEditScreen'
+import OrderListScreen from './screens/OrderListScreen'
 
 const App = () => {
   return (
@@ -33,6 +34,7 @@ const App = () => {
             <Route path='/register' element={<RegisterScreen />}/>
             <Route path='/profile' element={<ProfileScreen />}/>
             <Route path='/admin/userlist' element={<UserListScreen />}/>
+            <Route path='/admin/orderlist' element={<OrderListScreen />}/>
             <Route path='/admin/user/:id/edit' element={<UserEditScreen />}/>
             <Route path='/admin/productlist' element={<ProductListScreen />}/>
             <Route path='/admin/product/:id/edit' element={<ProductEditScreen />}/>
