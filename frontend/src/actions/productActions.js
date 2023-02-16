fff5c48f4a64fcb5815166968a2367eec49ffa20
hollyxj <hmj19a@fsu.edu>
2023-02-15 23:17:57 -0500

    Fixed respone typo in productActions.js

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,42 +1,44 @@
 import axios from 'axios'
 import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from "../constants/productConstants"
 
-export const listProducts = () => async(dispatch) => {
+export const listProducts = () => async (dispatch) => {
     try {
-        dispatch({ type: PRODUCT_LIST_REQUEST })
-
-        const { data } = await axios.get('/api/products')
-
-        dispatch({
-            type: PRODUCT_LIST_SUCCESS,
-            payload: data
-        })
+      dispatch({ type: PRODUCT_LIST_REQUEST })
+  
+      const { data } = await axios.get('/api/products')
+  
+      dispatch({
+        type: PRODUCT_LIST_SUCCESS,
+        payload: data,
+      })
     } catch (error) {
-        dispatch({
-            type: PRODUCT_LIST_FAIL,
-            payload: error.response && error.respone.data.message 
-            ? error.response.data.message 
+      dispatch({
+        type: PRODUCT_LIST_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
             : error.message,
-        })
+      })
     }
-}
+  }
 
-export const listProductDetails = (id) => async(dispatch) => {
+export const listProductDetails = (id) => async (dispatch) => {
     try {
-        dispatch({ type: PRODUCT_DETAILS_REQUEST })
-
-        const { data } = await axios.get(`/api/products/${id}`)
-
-        dispatch({
-            type: PRODUCT_DETAILS_SUCCESS,
-            payload: data
-        })
+      dispatch({ type: PRODUCT_DETAILS_REQUEST })
+  
+      const { data } = await axios.get(`/api/products/${id}`)
+  
+      dispatch({
+        type: PRODUCT_DETAILS_SUCCESS,
+        payload: data,
+      })
     } catch (error) {
-        dispatch({
-            type: PRODUCT_DETAILS_FAIL,
-            payload: error.response && error.respone.data.message 
-            ? error.response.data.message 
+      dispatch({
+        type: PRODUCT_DETAILS_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
             : error.message,
-        })
+      })
     }
-}
+  }
