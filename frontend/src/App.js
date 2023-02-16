dbe0353f900d2dedaa9a6fff4e038c30b4f1a18c
hollyxj <hmj19a@fsu.edu>
2023-02-15 21:46:25 -0500

    Order screen showing address & payment method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
             <Route path='/shipping' element={<ShippingScreen />}/>
             <Route path='/payment' element={<PaymentScreen />}/>
             <Route path='/placeorder' element={<PlaceOrderScreen />}/>
+            <Route path='/order/:id' element={<OrderScreen />}/>
             <Route path='/register' element={<RegisterScreen />}/>
             <Route path='/profile' element={<ProfileScreen />}/>
             <Route path='/product/:id' element={<ProductScreen />}/>
--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -39,6 +39,7 @@ export const saveShippingAddress = (data) => (dispatch) => {
 }
 
 export const savePaymentMethod = (data) => (dispatch) => {
+  console.log(data)
   dispatch({
     type: CART_SAVE_PAYMENT_METHOD,
     payload: data,
