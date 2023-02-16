eea7805e3f25d356601e68094211c3ab95ab008a
hollyxj <hmj19a@fsu.edu>
2023-02-15 22:35:19 -0500

    Show orders on profile - Backend

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems, getOrderByID, updateOrderToPaid } from '../controllers/orderController.js'
+import { addOrderItems, getOrderByID, updateOrderToPaid, getMyOrders } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
+router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderByID)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
 
