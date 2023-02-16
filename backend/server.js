2059557ae7b52d844ac73c3b778317f3efdfe99d
hollyxj <hmj19a@fsu.edu>
2023-02-16 14:05:02 -0500

    Image Upload Config & Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -6,6 +7,7 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -23,6 +25,7 @@ app.use('/api/products', productRoutes)
 app.use('/cart/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req,res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
@@ -31,6 +34,8 @@ const PORT = process.env.PORT || 4000
 
 app.listen(PORT), console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.magenta.bold)
 
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
