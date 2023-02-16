472fdb089dddcc6798ea853c1bf468510b56089d
hollyxj <hmj19a@fsu.edu>
2023-02-16 11:59:57 -0500

    User Edit Screen & Get User Details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
+import UserEditScreen from './screens/UserEditScreen'
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
             <Route path='/register' element={<RegisterScreen />}/>
             <Route path='/profile' element={<ProfileScreen />}/>
             <Route path='/admin/userlist' element={<UserListScreen />}/>
+            <Route path='/admin/user/:id/edit' element={<UserEditScreen />}/>
             <Route path='/product/:id' element={<ProductScreen />}/>
             <Route path='/cart'>
               <Route path=':id' element={<CartScreen />} />
