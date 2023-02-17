c0fc98afba60e4d0d85d496215397688d58adc9a
hollyxj <hmj19a@fsu.edu>
2023-02-17 01:20:38 -0500

    Fix shipping issue

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,11 +13,11 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
 
 const userInfoFromStorage = localStorage.getItem('userInfo') 
     ? JSON.parse(localStorage.getItem('userInfo'))
-    : null
+    : {}
 
 const shippingAddressFromStorage = localStorage.getItem('shippingAddress') 
     ? JSON.parse(localStorage.getItem('shippingAddress'))
-    : null
+    : {}
 
 const store = configureStore({
     reducer: {
