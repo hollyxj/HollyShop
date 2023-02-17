5e88782e15f2fcbd08c4a0a11c1fcac4277c2d6f
hollyxj <hmj19a@fsu.edu>
2023-02-17 00:43:40 -0500

    Prepare for deployment:

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -10,7 +10,7 @@ const errorHandler = (err, req, res, next) => {
     res.status(statusCode)
     res.json({
         message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? NULL : err.stack,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
     })
 }
 
